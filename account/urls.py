63d9d6d117d535d881f0dc8181055334096b2634
Pablo Leonczik <pablo.leonczik@gmail.com>
2022-11-12 20:01:35 -0300

    About

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from account import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', views.mi_login, name = 'login'),
     path('logout/', LogoutView.as_view(template_name='account/logout.html'), name = 'logout'),
-
+    path('about/', views.about, name='about'),
 ]
--- account/views.py
+++ account/views.py
@@ -14,3 +14,6 @@ def mi_login(request):
         formulario = AuthenticationForm()
     
     return render(request, 'account/login.html', {'formulario': formulario})
+
+def about(request):
+    return render(request, 'about.html', {} )
